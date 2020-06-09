e1526b1a6b6c9577b14c7d85268ac2e0a0c99807
MarioSimou <marios.simouu@gmail.com>
2020-06-09 22:34:14 +0100

    1st

---
+++ cmd/myapp/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/google/uuid"
+)
+
+func pong(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "pong\n")
+}
+
+func main(){
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	var address = fmt.Sprintf(":%s", port)
+
+	http.HandleFunc("/ping", pong)
+	log.Printf("The app listens on %s - process id: %s\n", address, uuid.New().String())
+	log.Fatalln(http.ListenAndServe(address, nil))
+}

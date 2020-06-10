5002a207b600d3ac474a9f1d4bb016c6450c9a00
MarioSimou <marios.simouu@gmail.com>
2020-06-10 19:37:18 +0100

    7th

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -17,7 +17,7 @@ func home(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "welcome home\n")
 }
 
-func name(w http.ResponseWriter, r *http.Request){
+func username(w http.ResponseWriter, r *http.Request){
 	var username = r.URL.Query().Get("name")
 	if username == "" {
 		fmt.Fprintf(w, "I don't know your name")
@@ -36,7 +36,7 @@ func main(){
 
 	http.HandleFunc("/ping", pong)
 	http.HandleFunc("/", home)
-	http.HandleFunc("/name", name)
+	http.HandleFunc("/username", username)
 
 	log.Printf("The app listens on %s - process id: %s\n", address, uuid.New().String())
 	log.Fatalln(http.ListenAndServe(address, nil))

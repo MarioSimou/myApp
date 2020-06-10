615160f3bb66815f569a8990d84052bb3ae978e1
MarioSimou <marios.simouu@gmail.com>
2020-06-10 22:57:48 +0100

    welcoem

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -12,6 +12,9 @@ import (
 func pong(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "pong\n")
 }
+func welcome(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "welcome\n")
+}
 
 func main(){
 	var port = os.Getenv("PORT")
@@ -22,6 +25,7 @@ func main(){
 	var address = fmt.Sprintf(":%s", port)
 
 	http.HandleFunc("/ping", pong)
+	http.HandleFunc("/welcome", welcome)
 
 	log.Printf("The app listens on %s - process id: %s\n", address, uuid.New().String())
 	log.Fatalln(http.ListenAndServe(address, nil))

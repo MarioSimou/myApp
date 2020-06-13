e2cf77d3eaacb37033458a36ca8981d2b1b09d30
MarioSimou <marios.simouu@gmail.com>
2020-06-13 19:08:15 +0100

    removing env show

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -16,14 +16,6 @@ func pong(w http.ResponseWriter, r *http.Request){
 func welcome(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "welcome\n")
 }
-func showEnv(w http.ResponseWriter, r *http.Request){
-	var envName = r.URL.Query().Get("q")
-	if v := os.Getenv(envName); envName != "" && v != ""  {
-		fmt.Fprintf(w, fmt.Sprintf("name: %s", v))
-	} else {
-		fmt.Fprintf(w, "No match for this variable")		
-	}
-}
 
 func main(){
 	var port = os.Getenv("PORT")
@@ -36,7 +28,6 @@ func main(){
 
 	http.HandleFunc("/ping", pong)
 	http.HandleFunc("/", welcome)
-	http.HandleFunc("/env", showEnv)
 
 	log.Printf("The app listens on %s - process id: %s\n", address, uuid.New().String())
 	log.Fatalln(http.ListenAndServe(address, nil))
